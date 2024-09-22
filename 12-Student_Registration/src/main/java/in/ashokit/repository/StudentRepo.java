d72384904b1fe4355bc79629006f4aeb28ab4d49
NagamSiva <102962708+NagamSiva@users.noreply.github.com>
2024-09-22 18:39:37 +0530

    Delete 12-Student_Registration directory

--- 12-Student_Registration/src/main/java/in/ashokit/repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package in.ashokit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.ashokit.entity.StudentEntity;
-
-public interface StudentRepo extends JpaRepository<StudentEntity, Integer> {
-
-}
